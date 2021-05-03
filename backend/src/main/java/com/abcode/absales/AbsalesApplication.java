a9512cb1f3f0cc7b5dd7248104e6783e4158c16e
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-05-03 12:42:24 -0400

    Project created

---
+++ backend/src/main/java/com/abcode/absales/AbsalesApplication.java
@@ -0,0 +1,13 @@
+package com.abcode.absales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsalesApplication.class, args);
+	}
+
+}
